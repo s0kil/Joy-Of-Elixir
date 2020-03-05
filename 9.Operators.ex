8cfb6d0ffa23718411c9f79706d7fbe295375810
Daniel Sokil <dtechtours@gmail.com>
2020-03-05 10:45:33 -0500

    Operators

---
+++ 9.Operators.ex
@@ -0,0 +1,36 @@
+# Some Math
+1 + 1 == 2
+10 - 5 == 5
+5 * 2 == 10
+
+# `/` Always Returns A Float
+10 / 5 == 5.0
+
+# Integer Division
+div(10, 2) == 5
+
+# Remainder Of Division
+rem(10, 3) == 1
+
+# Boolean Operators, Expecting A Boolean As First Argument
+true and true == true
+false or true == true
+
+1 || true == 1
+false && 1 == false
+nil && 20 == nil
+!true == false
+
+# Comparisons
+1 == 1
+1 != 1
+1 < 2
+
+# Strict Comparison
+1 == 1.0  # True
+1 === 1.0 # False
+
+# Compare Data Types
+1 < :hello == true
+
+# Sorting Order: number < atom < reference < functions < port < pid < tuple < list < bit string
